c0f81a2fc1a7a6badd40b5f523e355728956cfb4
sreetharraj <sreetharradha188@gmail.com>
2024-02-13 15:48:16 +0530

    add some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Product p = new Product();
+
+        int ans = p.product(5,6);
+
+        int ans1 =p.product(5,6,7);
+
+        double ans2 =p.product(5.6,6.7);
+    }
+    static class Product{
+
+        public int product(int x, int y)
+        {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z)
+        {
+            return x*y*z;
+        }
+
+        public double product(double x, double y)
+        {
+            return x*y;
+        }
+    }
 }
